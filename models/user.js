4fc5e48dcb20dbf0fc22544aabbde788089c4ee6
Vitor Manuel <chorimano.vitor@hotmail.com>
2024-06-17 20:11:25 -0300

    new file:   config/passportConfig.js 	new file:   migrations/20240617230542-create-user.js 	new file:   models/user.js 	modified:   node_modules/.package-lock.json 	new file:   node_modules/passport-local/LICENSE 	new file:   node_modules/passport-local/README.md 	new file:   node_modules/passport-local/lib/index.js 	new file:   node_modules/passport-local/lib/strategy.js 	new file:   node_modules/passport-local/lib/utils.js 	new file:   node_modules/passport-local/package.json 	new file:   node_modules/passport-strategy/.jshintrc 	new file:   node_modules/passport-strategy/.travis.yml 	new file:   node_modules/passport-strategy/LICENSE 	new file:   node_modules/passport-strategy/README.md 	new file:   node_modules/passport-strategy/lib/index.js 	new file:   node_modules/passport-strategy/lib/strategy.js 	new file:   node_modules/passport-strategy/package.json 	new file:   node_modules/passport/CHANGELOG.md 	new file:   node_modules/passport/LICENSE 	new file:   node_modules/passport/README.md 	new file:   node_modules/passport/etc/jsdoc.json 	new file:   node_modules/passport/lib/authenticator.js 	new file:   node_modules/passport/lib/errors/authenticationerror.js 	new file:   node_modules/passport/lib/framework/connect.js 	new file:   node_modules/passport/lib/http/request.js 	new file:   node_modules/passport/lib/index.js 	new file:   node_modules/passport/lib/middleware/authenticate.js 	new file:   node_modules/passport/lib/middleware/initialize.js 	new file:   node_modules/passport/lib/sessionmanager.js 	new file:   node_modules/passport/lib/strategies/session.js 	new file:   node_modules/passport/package.json 	new file:   node_modules/passport/sponsors/descope-dark.svg 	new file:   node_modules/passport/sponsors/descope.svg 	new file:   node_modules/passport/sponsors/fusionauth.png 	new file:   node_modules/passport/sponsors/fusionauth.svg 	new file:   node_modules/passport/sponsors/loginradius.png 	new file:   node_modules/passport/sponsors/snyk.png 	new file:   node_modules/passport/sponsors/workos.png 	new file:   node_modules/pause/.npmignore 	new file:   node_modules/pause/History.md 	new file:   node_modules/pause/Makefile 	new file:   node_modules/pause/Readme.md 	new file:   node_modules/pause/index.js 	new file:   node_modules/pause/package.json 	modified:   package-lock.json 	modified:   package.json

---
+++ models/user.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    nome: DataTypes.STRING,
+    email: DataTypes.STRING,
+    senha: DataTypes.STRING,
+    role: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN,
+    isIntern: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
